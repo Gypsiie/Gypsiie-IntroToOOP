33673643cdf0e3ac3232fba90edd8adb190a2170
Kachi Ogbodo <kachie.ogbodo@gmail.com>
2023-03-07 22:02:30 +0000

    Completed - Instantiation

--- src/test/java/BirdTest.java
+++ src/test/java/BirdTest.java
@@ -12,19 +12,21 @@ public class BirdTest {
         b1.ageMonths = 0;
         String expected = "Tweet";
         String actual = b1.sound();
-        Assert.assertEquals(expected, actual);
-    }
+        Assert.assertEquals(expected, actual); 
+     }
+    
     /**
      * If ageMonths is less than 3, birds should say "Chirp".
      */
     @Test
     public void birdTest2(){
         Bird b1 = new Bird();
-        b1.ageMonths = 4;
+        b1.ageMonths = 4; 
         String expected = "Chirp";
         String actual = b1.sound();
         Assert.assertEquals(expected, actual);
     }
+    
     /**
      * If ageMonths is less than 3, birds should say "Chirp".
      */
